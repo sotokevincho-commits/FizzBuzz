a48eeb3bece2947d408c8388eec0ee293e34d0eb
KevinAndresSotoLombana <sotokevincho@gmail.com>
2026-07-01 10:20:25 -0500

    ejercicio fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,11 @@
+for(let numero = 1; numero <= 100; numero++) {
+    if (numero % 3 == 0 && numero % 5 == 0)
+        console.log("FizzBuzz");
+    else if (numero % 3 == 0)
+        console.log("Fizz");
+    else if (numero % 5 == 0)
+        console.log("Buzz");
+    else {
+        console.log(numero);
+    }
+}
